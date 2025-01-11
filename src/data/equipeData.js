4c2457db2adda5489e716f69a36335e0b74e3c23
HubertTruong <archisabatier@gmail.com>
2025-01-11 01:51:15 +0100

    add equipe

---
+++ src/data/equipeData.js
@@ -0,0 +1,82 @@
+export const equipeData = [
+  {
+    name: 'Félix',
+    image: '/images/equipe/felix.jpg',
+    job: ['Directeur Musique'],
+  },
+  {
+    name: 'Saturnin',
+    image: '/images/equipe/saturnin.jpg',
+    job: ['Respo logistique'],
+  },
+  {
+    name: 'Samuel',
+    image: '/images/equipe/samuel.jpg',
+    job: ['Directeur technique'],
+  },
+  {
+    name: 'Antoine',
+    image: '/images/equipe/antoine.jpg',
+    job: ['Président', 'Fondateur'],
+  },
+  {
+    name: 'Julien',
+    image: '/images/equipe/julien.jpg',
+    job: ['Vice-président', 'Co-fondateur'],
+  },
+  {
+    name: 'Sybil',
+    image: '/images/equipe/sybil.jpg',
+    job: ['Directrice de prod'],
+  },
+  {
+    name: 'Noé',
+    image: '/images/equipe/noe.jpg',
+    job: ['Scénographe'],
+  },
+  {
+    name: 'Constant',
+    image: '/images/equipe/constant.jpg',
+    job: ['Respo exposition'],
+  },
+  {
+    name: 'Charlotte',
+    image: '/images/equipe/charlotte.jpg',
+    job: ['Respo com'],
+  },
+  {
+    name: 'Clara',
+    image: '/images/equipe/clara.jpg',
+    job: ['Chargée de sub'],
+  },
+  {
+    name: 'Lise',
+    image: '/images/equipe/lise.jpg',
+    job: ['Chargée de com'],
+  },
+  {
+    name: 'Melissa',
+    image: '/images/equipe/melissa.jpg',
+    job: ['Respo log'],
+  },
+  {
+    name: 'Mathis',
+    image: '/images/equipe/mathis.jpg',
+    job: ['Respo village'],
+  },
+  {
+    name: 'Matthis',
+    image: '/images/equipe/matthis.jpg',
+    job: ['Directeur musique'],
+  },
+  {
+    name: 'Gustave',
+    image: '/images/equipe/gustave.jpg',
+    job: ['Respo logistique'],
+  },
+  {
+    name: 'Hannah',
+    image: '/images/equipe/hannah.jpg',
+    job: ['Directrice artistique'],
+  },
+];
--- src/pages/Equipe.js
+++ src/pages/Equipe.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import TitlePage from '../components/atoms/TitlePage';
 import styles from '../styles/Equipe.module.css';
+import { equipeData } from '../data/equipeData';
 
 export default function Equipe() {
+  const equipe = equipeData;
+
   return (
     <div className={styles.container}>
       <TitlePage label="l'équipe" />
+      <div className={styles.containerImages}>
+        {equipe.map((person, index) => (
+          <div className={styles.containerImage} key={index}>
+            <img src={person.image} alt={person.name} className={styles.img} />
+            <p className={styles.text}>{person.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
